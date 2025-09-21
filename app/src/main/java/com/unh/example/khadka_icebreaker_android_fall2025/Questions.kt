e36672326cf2b82871ba179155114a3c4ae0144f
khadkaashish <ashishkhadka03@gmail.com>
2025-09-21 17:35:39 -0400

    Created question model

---
+++ app/src/main/java/com/unh/example/khadka_icebreaker_android_fall2025/Questions.kt
@@ -0,0 +1,6 @@
+package com.unh.example.khadka_icebreaker_android_fall2025
+
+data class Questions(
+    val text: String = "", // Will come from the db
+    val id: String = "" // unique hash id within db
+)
